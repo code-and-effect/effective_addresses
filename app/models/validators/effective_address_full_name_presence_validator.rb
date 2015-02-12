cc5b7f11ca574da2e4a6ab645a9a83d5b4cb7d89
Matt Riemer <matthew@agilestyle.com>
2015-02-12 12:04:28 -0700

    tweak use_full_name validation

--- app/models/validators/effective_address_full_name_presence_validator.rb
+++ app/models/validators/effective_address_full_name_presence_validator.rb
@@ -1,8 +1,8 @@
 class EffectiveAddressFullNamePresenceValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if value.present? && value.full_name.blank?
+    if value.present? && !value.empty? && value.full_name.blank?
       record.errors[attribute] << "is invalid"
-      value.errors[:full_name] << "can't be blank" 
+      value.errors[:full_name] << "can't be blank"
     end
   end
 end
